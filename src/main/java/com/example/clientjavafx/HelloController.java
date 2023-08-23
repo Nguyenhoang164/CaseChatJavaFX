352c880f3d5e281988d2ed10ba8b3dcc8da89931
PC <Kamito062004@gmail.com>
2023-08-23 11:59:31 +0700

    Client

--- src/main/java/com/example/clientjavafx/HelloController.java
+++ src/main/java/com/example/clientjavafx/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.clientjavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -10,63 +9,85 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.sql.*;
-import java.util.Scanner;
 
 public class HelloController {
     @FXML
     private Label Message;
     @FXML
     private TextField Input_Message;
+    private Socket serverSocket;
+
     @FXML
     public void AddMessage() throws IOException, SQLException {
-        HelloController helloController = new HelloController();
+        ConnectedServer();
         String Message = this.Input_Message.getText();
-        Socket socket = helloController.socket();
         ImportToDatabase importToDatabase = new ImportToDatabase();
-        String message = SendMessageToServer(socket,Message);
-        if (message.equals("exit")){
+        String message = SendMessageToServer(Message);
+        if (message.isEmpty()) {
+            StopConnected();
             importToDatabase.DeleteAll();
         }
         importToDatabase.AddToDatabase(message);
-        ReadFromServer(socket);
+        ReadFromServer();
         StringBuilder listMessage = importToDatabase.ReadMessage();
         this.Message.setText(String.valueOf(listMessage));
         this.Input_Message.setText("");
-
-        }
-        public Socket socket() throws IOException {
-            String localhost = "localhost";
-            int ClientPort = 8080;
-            Socket socket = new Socket(localhost, ClientPort);
-            return socket;
-        }
-
-    private static String ReadFromServer(Socket socket) throws IOException {
-        InputStream inputStream = socket.getInputStream();
-        byte[] list = new byte[1024];
-        int read = inputStream.read(list);
-        String message = new String(list, 0, read);
-        return message;
     }
-    private static String SendMessageToServer(Socket socket, String message) throws IOException {
-        OutputStream outputStream = socket.getOutputStream();
-        String messageLine = "Client : " + message;
-        outputStream.write(messageLine.getBytes());
-        outputStream.flush();
-        return messageLine;
+   @FXML
+   public void DeleteMessage() throws SQLException{
+        ImportToDatabase importToDatabase = new ImportToDatabase();
+        importToDatabase.DeleteAll();
+        this.Message.setText("");
+   }
+    public void ConnectedServer() throws IOException {
+        String localhost = "localhost";
+        int ClientPort = 2121;
+        serverSocket = new Socket(localhost, ClientPort);
+        System.out.println("ket noi thanh cong voi server");
+    }
+
+
+    private String ReadFromServer() throws IOException {
+        if (serverSocket != null && serverSocket.isConnected()) {
+            InputStream inputStream = this.serverSocket.getInputStream();
+            byte[] list = new byte[1024];
+            int read = inputStream.read(list);
+            String message = new String(list, 0, read);
+            return message;
+        }
+        return "";
+    }
+
+    private String SendMessageToServer(String message) throws IOException {
+        if (serverSocket != null && serverSocket.isConnected()) {
+            OutputStream outputStream = this.serverSocket.getOutputStream();
+            String messageLine = "Client : " + message;
+            outputStream.write(messageLine.getBytes());
+            outputStream.flush();
+            return messageLine;
+        }
+        return "";
+    }
+
+    public void StopConnected() throws IOException {
+        if (serverSocket != null && !serverSocket.isClosed()) {
+            serverSocket.close();
+        }
     }
 }
-class ImportToDatabase{
+
+class ImportToDatabase {
     private String localhost = "localhost:3306";
     private String dbname = "DataMessage";
     private String username = "root";
     private String password = "Kamito@123";
-    private String URLConnect ="jdbc:mysql://"+ localhost+"/"+dbname;
+    private String URLConnect = "jdbc:mysql://" + localhost + "/" + dbname;
+
     public Connection ConnectToDatabase() throws SQLException {
-        Connection connection = DriverManager.getConnection(URLConnect,username,password);
-        System.out.println("Kết nối database thành công ");
+        Connection connection = DriverManager.getConnection(URLConnect, username, password);
         return connection;
     }
+
     public void AddToDatabase(String message) throws SQLException {
         ImportToDatabase importToDatabase = new ImportToDatabase();
         Connection connection = importToDatabase.ConnectToDatabase();
@@ -75,6 +96,7 @@ class ImportToDatabase{
         Statement statement = connection.createStatement();
         statement.executeUpdate(query);
     }
+
     public void DeleteAll() throws SQLException {
         ImportToDatabase importToDatabase = new ImportToDatabase();
         Connection connection = importToDatabase.ConnectToDatabase();
@@ -82,7 +104,8 @@ class ImportToDatabase{
         Statement statement = connection.createStatement();
         statement.executeUpdate(query);
     }
-    public StringBuilder ReadMessage() throws SQLException{
+
+    public StringBuilder ReadMessage() throws SQLException {
         ImportToDatabase importToDatabase = new ImportToDatabase();
         Connection connection = importToDatabase.ConnectToDatabase();
         String query = "select Message from Message";
@@ -90,7 +113,7 @@ class ImportToDatabase{
         ResultSet resultSet = statement.executeQuery(query);
         StringBuilder newMessage = new StringBuilder();
         String message = null;
-        while (resultSet.next()){
+        while (resultSet.next()) {
             message = resultSet.getString("Message");
             newMessage.append(message + "\n");
         }
